13ce812466565fd7b39dfab0cb93423fe479f6f9
Yomiyu15 <yomiwaaq@gmail.com>
2025-12-23 10:54:28 +0300

    updates

--- db/index.js
+++ db/index.js
@@ -14,12 +14,18 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // REQUIRED for Render
-  },
-});
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }, // needed for cloud-hosted Postgres
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
 
 module.exports = pool;
 
